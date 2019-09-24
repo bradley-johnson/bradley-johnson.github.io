adcc94b850d2fa1a555859d7f20b2c57e80e3f4a
Johnson <bdj42@txstate.edu>
2019-09-23 19:58:35 -0500

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,13 @@
-x = 22;
-const y = 33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33);
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+function condition()
+{
+    x = confirm("Are you sure you want to proceed?");
+    if(x)
+    {
+        document.writeln("You chose Okay!");
+    }
+    else
+    {
+        document.writeln("You chose Cancel!");
+    }
+}
