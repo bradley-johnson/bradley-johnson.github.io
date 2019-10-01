efea21fcd47083eddba619ae1ff772b70e3c28e1
Johnson <bdj42@txstate.edu>
2019-09-30 19:42:34 -0500

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,16 +1,14 @@
-/*document.writeln("<button onclick='condition()'>Conditional Test</button>")
-function condition()
+function welcome()
 {
-    x = confirm("Are you sure you want to proceed?");
-    if(x)
-    {
-        document.writeln("You chose Okay!");
-    }
-    else
-    {
-        document.writeln("You chose Cancel!");
-    }
-}*/
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
 
-name = window.prompt("Please enter your name", "Type your name here");
-document.write(name);
+  // A prompt box is used to prompt users to input a value before entering a page.
+  user_name = window.prompt(a, b);
+
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+
+  return message;
+}
+
+document.write(welcome());
